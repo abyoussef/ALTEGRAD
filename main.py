f8612a91a96b8b8f8f50bd714ce052c76d0d3637
BoeingX <song.baoyang@gmail.com>
2017-03-06 23:11:53 +0100

    introduced a method consists of reranking results of baseline method by tf-idf score

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from helpers.misc import train_test_split, score, make_X_y, write_to_file, clean
 from methods.baseline import baseline
 from methods.tfidf_centroid import tfidf_centroid
 from methods.tfidf import tfidf
+from methods.baseline_tfidf import baseline_tfidf
 
 def test(method, cv = None):
     path_to_data = 'Data/'
@@ -51,5 +52,5 @@ def submission(method):
     write_to_file(y_pred, os.path.join(path_to_data, method.__name__ + '.csv'))
 
 if __name__ == '__main__':
-    test(tfidf, cv = 5)
-    #submission(tfidf_centroid)
+    #test(tfidf, cv = 5)
+    submission(tfidf_centroid)
--- methods/baseline_tfidf.py
+++ methods/baseline_tfidf.py
@@ -1,6 +1,19 @@
 import pandas as pd
 from pandas import Series, DataFrame
-from scipy.sparse import vstack
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from helpers.misc import split_cell, top_k_score
+from methods.baseline import baseline_score
+from methods.tfidf import tfidf_score
+
+def baseline_tfidf(X_train, y_train, X_test):
+    """Use baseline method but sort with respect to TF-IDF similarity."""
+    scores_freq = baseline_score(X_train.copy(), y_train.copy(), X_test.copy())
+    scores_sim = tfidf_score(X_train.copy(), y_train.copy(), X_test.copy())
+    scores_freq.rename(columns = {'score': 'score_freq'}, inplace = True)
+    scores_sim.rename(columns = {'score': 'score_sim'}, inplace = True)
+    scores = pd.merge(left = scores_freq, right = scores_sim, on = ['mid', 'recipients'], how = 'inner')
+
+    scores.sort_values(['mid', 'score_freq'], inplace = True, ascending = [True, False])
+    scores = scores.groupby('mid').head(10).reset_index(drop = True)
+
+    y_pred = scores.sort_values(['mid', 'score_sim'], ascending = [True, False])[['mid', 'recipients']]
+    y_pred = y_pred.groupby('mid')['recipients'].apply(lambda x: ' '.join(x)).reset_index()
+    return y_pred
